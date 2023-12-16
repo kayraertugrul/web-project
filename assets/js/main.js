cc7f96e9fa03e28ec3bcb4f8184f24f2b211f9a1
kertugrul <kertugrul@hotech.systems>
2023-12-16 20:57:47 +0300

    #daha fazla temizlik

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,42 +172,6 @@
   });
 
   /**
-   * Clients Slider
-   */
-  new Swiper(".clients-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60,
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80,
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120,
-      },
-    },
-  });
-
-  /**
    * Skills animation
    */
   let skilsContent = select(".skills-content");
@@ -261,23 +225,6 @@
    */
   const portfolioLightbox = GLightbox({
     selector: ".portfolio-lightbox",
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper(".recipe-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
   });
 
   /**
